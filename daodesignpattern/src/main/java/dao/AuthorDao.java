05fb5269e2a79d205ad015eca687bc154e828356
rachidesper <rachidesper290@gmail.com>
2026-04-01 09:14:17 -0300

    Implementation of RowMapper

--- daodesignpattern/src/main/java/dao/AuthorDao.java
+++ daodesignpattern/src/main/java/dao/AuthorDao.java
@@ -2,7 +2,12 @@ package dao;
 
 import domain.Author;
 
+import java.util.Optional;
+
 public interface AuthorDao {
 
     void create (Author author);
+
+    Optional<Author> findOne(long l);
+
 }
--- daodesignpattern/src/main/java/dao/impl/AuthorDaoImpl.java
+++ daodesignpattern/src/main/java/dao/impl/AuthorDaoImpl.java
@@ -2,6 +2,12 @@ package dao.impl;
 import dao.AuthorDao;
 import domain.Author;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 public class AuthorDaoImpl  implements AuthorDao {
 
@@ -15,5 +21,24 @@ public class AuthorDaoImpl  implements AuthorDao {
     public void create(Author author) {
         String sql = "INSERT INTO authors (id, name, age) VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, author.getId(), author.getName(), author.getAge());
+    }
+
+    @Override
+    public Optional<Author> findOne(long authorId) {
+        String sql = "SELECT * FROM authors WHERE id = ?";
+        List<Author> authors =jdbcTemplate.query(sql, new AuthorRowMapper(), authorId);
+         return authors.stream().findFirst();
+    }
+
+    // Me convierte una fila de registro SQL en un objeto con sus respectivos campos
+    public static class AuthorRowMapper implements RowMapper<Author> {
+        @Override
+        public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return Author.builder().
+                    id(rs.getLong("id"))
+                    .name(rs.getNString("name"))
+                    .age(rs.getInt("age"))
+                    .build();
+        }
     }
 }
